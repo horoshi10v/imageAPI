1e99870d4d44ef711078310c4303c467270ec608
horoshi10v <valentyn.khoroshylov@gmail.com>
2022-06-05 01:06:40 +0300

    check if resources directory exists

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"api/routes"
+	"errors"
+	"log"
+	"os"
+)
+
+const (
+	pathFiles = "resources"
 )
 
 func main() {
+	//make directory if not exists
+	if _, err := os.Stat(pathFiles); errors.Is(err, os.ErrNotExist) {
+		err := os.Mkdir(pathFiles, os.ModePerm)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 	routes.Setup()
 }
